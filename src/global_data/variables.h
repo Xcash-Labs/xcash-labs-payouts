896fc66a9768f8ed5290435c3afaa6f4e2c2366b
John D <az0006t@protonmail.com>
2025-02-12 22:14:55 -0500

    Update variables.h

--- src/global_data/variables.h
+++ src/global_data/variables.h
@@ -11,7 +11,7 @@ extern bool is_seed_node;   // True if node is a seed node
 extern char xcash_wallet_public_address[XCASH_WALLET_LENGTH+1]; // Holds your wallets public address
 extern char XCASH_DPOPS_delegates_IP_address[BLOCK_VERIFIERS_IP_ADDRESS_TOTAL_LENGTH]; // The  block verifiers IP address to run the server on
 extern char XCASH_daemon_IP_address[BLOCK_VERIFIERS_IP_ADDRESS_TOTAL_LENGTH]; // The XCASH daemon IP
-char XCASH_wallet_IP_address[BLOCK_VERIFIERS_IP_ADDRESS_TOTAL_LENGTH]; // The  wallet IP address
+extern char XCASH_wallet_IP_address[BLOCK_VERIFIERS_IP_ADDRESS_TOTAL_LENGTH]; // The  wallet IP address
 extern char MongoDB_uri[256];
 
 #endif
