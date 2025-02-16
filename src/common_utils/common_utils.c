28bb1b8d2be0e7cf13c4ca27dabdb0a6d7919e77
John D <az0006t@protonmail.com>
2025-02-15 22:24:00 -0500

    Update common_utils.c

--- src/common_utils/common_utils.c
+++ src/common_utils/common_utils.c
@@ -6,12 +6,17 @@
 void log_message(int level, const char *function, const char *format, ...)
 {
     va_list args;
+    char buffer[1024];
+
     va_start(args, format);
+    vsnprintf(buffer, sizeof(buffer), format, args); // Format message
+    va_end(args);
     if (level == LOG_ERR || level == LOG_DEBUG)
     {
-        fprintf(stderr, "%s: ", function);
-        vfprintf(stderr, format, args);
+//        fprintf(stderr, "%s: ", function);
+//        vfprintf(stderr, format, args);
 //        fprintf(stderr, "\n");
+        fprintf(stderr, "%s: %s", function, buffer);
     }
-    va_end(args);
+//    va_end(args);
 }
