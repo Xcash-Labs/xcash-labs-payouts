789aae7add7e09b8a18344400fb9dfe641609088
John D <az0006t@protonmail.com>
2025-02-17 15:12:29 -0500

    Update db_functions.h

--- src/db/db_functions.h
+++ src/db/db_functions.h
@@ -5,7 +5,7 @@
 #include <bson/bson.h>
 #include <stdbool.h>
 
-bool initialize_database(const char* mongo_uri);
+bool initialize_database(void);
 void shutdown_database(void);
 bool initialize_mongo_database(const char* mongo_uri, mongoc_client_pool_t** database_client_thread_pool);
 void shutdown_mongo_database(mongoc_client_pool_t** database_client_thread_pool);
