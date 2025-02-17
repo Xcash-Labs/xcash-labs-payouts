ce0d0a69973a33c81c4fd9242e7a90f9de8cd991
John D <az0006t@protonmail.com>
2025-02-17 18:48:48 -0500

    Update

--- src/db/db_init.c
+++ src/db/db_init.c
@@ -22,8 +22,8 @@ bool initialize_mongo_database(const char *mongo_uri, mongoc_client_pool_t **db_
     // Create a new URI object from the provided URI string
     uri_thread_pool = mongoc_uri_new_with_error(mongo_uri, &error);
     if (!uri_thread_pool) {
-        snprintf(ws_buf, sizeof(wsbuf), "Failed to parse URI: %s\nError message: %s", mongo_uri, error.message", mongo_uri, error.message);
-        HANDLE_DEBUG(wsbuff);
+        snprintf(wsbuf, sizeof(wsbuf), "Failed to parse URI: %s\nError message: %s", mongo_uri, error.message", mongo_uri, error.message);
+        HANDLE_DEBUG(wsbuf);
         return false;
     }
     // Create a new client pool with the parsed URI object
