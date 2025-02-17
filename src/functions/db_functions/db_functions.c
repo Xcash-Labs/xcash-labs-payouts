c9bcf014172be24d514a368a39ce282bfeae78ca
John D <az0006t@protonmail.com>
2025-02-17 09:55:19 -0500

    Update db_functions.c

--- src/functions/db_functions/db_functions.c
+++ src/functions/db_functions/db_functions.c
@@ -3,7 +3,6 @@
 #include "config.h"
 
 bool initialize_mongo_database(const char *mongo_uri, mongoc_client_pool_t **db_client_thread_pool) {
-    char tmpmsg[256];  
     mongoc_uri_t *uri_thread_pool;
     bson_error_t error;
     // Initialize the MongoDB client library
@@ -11,11 +10,9 @@ bool initialize_mongo_database(const char *mongo_uri, mongoc_client_pool_t **db_
     // Create a new URI object from the provided URI string
     uri_thread_pool = mongoc_uri_new_with_error(mongo_uri, &error);
     if (!uri_thread_pool) {
-        snprintf(tmpmsg, sizeof(tmpmsg), "Failed to parse URI: %s\nError message: %s\n", mongo_uri, error.message);
-        HANDLE_DEBUG(tmpmsg);
+//        HANDLE_DEBUG("Failed to parse URI: %s\nError message: %s\n", mongo_uri, error.message);
         return false;
     }
-
     // Create a new client pool with the parsed URI object
     *db_client_thread_pool = mongoc_client_pool_new(uri_thread_pool);
     if (!*db_client_thread_pool) {
