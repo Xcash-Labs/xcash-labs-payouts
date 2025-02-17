da3421b58973811261608e1ff69f958ab1894745
John D <az0006t@protonmail.com>
2025-02-17 09:45:56 -0500

    Update db_functions.c

--- src/functions/db_functions/db_functions.c
+++ src/functions/db_functions/db_functions.c
@@ -10,14 +10,14 @@ bool initialize_mongo_database(const char *mongo_uri, mongoc_client_pool_t **db_
     // Create a new URI object from the provided URI string
     uri_thread_pool = mongoc_uri_new_with_error(mongo_uri, &error);
     if (!uri_thread_pool) {
-        HANDLE_DEBUG(stderr, "Failed to parse URI: %s\nError message: %s\n", mongo_uri, error.message);
+        HANDLE_DEBUG("Failed to parse URI: %s\nError message: %s\n", mongo_uri, error.message);
         return false;
     }
 
     // Create a new client pool with the parsed URI object
     *db_client_thread_pool = mongoc_client_pool_new(uri_thread_pool);
     if (!*db_client_thread_pool) {
-        HANDLE_DEBUG(stderr, "Failed to create a new client pool.");
+        HANDLE_DEBUG("Failed to create a new client pool.");
         mongoc_uri_destroy(uri_thread_pool);
         return false;
     }
