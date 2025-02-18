354d90b7088253161362a2afc13101aec9e9a2c7
John D <az0006t@protonmail.com>
2025-02-17 20:41:35 -0500

    Update logger.c

--- src/utils/logger.c
+++ src/utils/logger.c
@@ -16,11 +16,13 @@ void log_debug_buffer(const char *function, const char *format, ...) {
     char buffer[LOG_BUFFER_LEN + 11];  // Single buffer with extra space for color codes
     va_list args;
     va_start(args, format);
-    int message_length = vsnprintf(buffer + 7, sizeof(buffer) - 11, format, args);  // Leave space for "\033[1;33m"
+    vsnprintf(buffer + 7, sizeof(buffer) - 11, format, args);  // Leave space for "\033[1;33m"
     va_end(args);
+
     snprintf(buffer, sizeof(buffer), "\033[1;33m%s\033[0m", buffer + 7);
     log_message(LOG_DEBUG, function, "%s", buffer);
 }
+
 
 
 void log_message(int level, const char *function, const char *format, ...)
