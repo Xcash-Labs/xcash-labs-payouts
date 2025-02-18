976e00a19a008d2d0e38a8978e5e2b2a552a3140
John D <az0006t@protonmail.com>
2025-02-17 19:30:27 -0500

    updates

--- src/db/db_init.c
+++ src/db/db_init.c
@@ -23,7 +23,7 @@ bool initialize_mongo_database(const char *mongo_uri, mongoc_client_pool_t **db_
     uri_thread_pool = mongoc_uri_new_with_error(mongo_uri, &error);
     if (!uri_thread_pool) {
 //        snprintf(wsbuf, sizeof(wsbuf), "Failed to parse URI: %s\nError message: %s", mongo_uri, error.message);
-        HANDLE_DEBUG("Failed to parse the mongo db URI");
+//        HANDLE_DEBUG("Failed to parse the mongo db URI");
         HANDLE_DEBUG_BUFFER(wsbuf);
         return false;
     }
