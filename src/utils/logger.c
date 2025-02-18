178cf01623d56ccfbb2d6da81171fb558c1cc56e
John D <az0006t@protonmail.com>
2025-02-17 22:19:38 -0500

    Update logger.c

--- src/utils/logger.c
+++ src/utils/logger.c
@@ -23,7 +23,7 @@ void log_debug_buffer(const char *function, const char *format, ...) {
     log_message(LOG_DEBUG, function, "%s", buffer);
 }
 
-void log_message(int level, const char *function, const char *format, ...) {
+void logger(int level, const char *function, const char *format, ...) {
     char buffer[LOG_BUFFER_LEN];  // No extra space needed for color
     va_list args;
     va_start(args, format);
@@ -41,20 +41,6 @@ void log_message(int level, const char *function, const char *format, ...) {
 }
 
 
-
-
-void logger(int level, const char *function, const char *format, ...) {
-    char buffer[LOG_BUFFER_LEN + 11];  // Single buffer with extra space for color codes
-    va_list args;
-    va_start(args, format);
-    vsnprintf(buffer + 7, sizeof(buffer) - 11, format, args);  // Leave space for "\033[1;33m"
-    va_end(args);
-
-    snprintf(buffer, sizeof(buffer), "\033[1;33m%s\033[0m", buffer + 7);
-    log_message(LOG_ERROR, function, "%s", buffer);
-    
-
-}
 
 void log_message(int level, const char *function, const char *format, ...)
 {
