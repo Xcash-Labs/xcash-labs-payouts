158ea462cdc4c0302fb55c47edf5d98964f792ea
John D <az0006t@protonmail.com>
2025-02-17 23:16:55 -0500

    Update logger.c

--- src/utils/logger.c
+++ src/utils/logger.c
@@ -26,7 +26,7 @@ void logger(int level, const char *function, const char *format, ...)
     }
     if (level == LOG_ERR)
     {
-        fprintf("\n");
+        fprintf(stderr, "\n");
         exit(EXIT_FAILURE);
     }
 }
