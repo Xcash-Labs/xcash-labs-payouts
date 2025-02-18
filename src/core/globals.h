75b8b15b92d5ec01fc157876c3478a8c87e69758
John D <az0006t@protonmail.com>
2025-02-18 09:02:57 -0500

    Updates

--- src/core/globals.h
+++ src/core/globals.h
@@ -11,6 +11,8 @@ Global Variables
 extern mongoc_client_pool_t* database_client_thread_pool;  // database
 
 extern bool debug_enabled;  // True if debug enabled
+extern int sig_requests; // for shutdown for signal requests
+
 
 extern bool is_seed_node;   // True if node is a seed node
 extern char XCASH_DPOPS_delegates_IP_address[IP_LENGTH+1]; // The  block verifiers IP address to run the server on
