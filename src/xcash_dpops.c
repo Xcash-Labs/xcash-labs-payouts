43e5eed71d0b25bd3a25f9a6d89539a910b3c16f
John D <az0006t@protonmail.com>
2025-02-18 09:17:23 -0500

    Update xcash_dpops.c

--- src/xcash_dpops.c
+++ src/xcash_dpops.c
@@ -209,7 +209,7 @@ int main(int argc, char *argv[])
   {
     INFO_PRINT("Shutting down. Threads remains %d", 2);
 
-    FATAL_ERROR_EXIT(LOG_ERR, __func__, "The --block-verifiers-secret-key is mandatory and should be %d characters long!", VRF_SECRET_KEY_LENGTH);
+    FATAL_ERROR_EXIT("The --block-verifiers-secret-key is mandatory and should be %d characters long!", VRF_SECRET_KEY_LENGTH);
 
 
 
