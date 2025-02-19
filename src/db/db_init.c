b0e43aba032118216c35ba8818caa10501624ad2
John D <az0006t@protonmail.com>
2025-02-18 19:50:33 -0500

    Update db_init.c

--- src/db/db_init.c
+++ src/db/db_init.c
@@ -1,6 +1,5 @@
 #include "db_init.h"
 #include "globals.h"
-#include "logger.h"
 #include <mongoc/mongoc.h>
 #include "config.h"
 
@@ -21,13 +20,13 @@ bool initialize_mongo_database(const char *mongo_uri, mongoc_client_pool_t **db_
     // Create a new URI object from the provided URI string
     uri_thread_pool = mongoc_uri_new_with_error(mongo_uri, &error);
     if (!uri_thread_pool) {
-        logger(LOG_ERR, __func__, "Failed to parse URI: %s\nError message: %s", mongo_uri, error.message);
+        INFO_PRINT("Failed to parse URI: %s\nError message: %s", mongo_uri, error.message);
         return false;
     }
     // Create a new client pool with the parsed URI object
     *db_client_thread_pool = mongoc_client_pool_new(uri_thread_pool);
     if (!*db_client_thread_pool) {
-        logger(LOG_DEBUG, __func__, "Failed to create a new client pool.");
+        INFO_PRINT("Failed to create a new client pool.");
         mongoc_uri_destroy(uri_thread_pool);
         return false;
     }
