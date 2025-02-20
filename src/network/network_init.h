e36dd0660e2b24c9bf677b6dc0681a2e7303f792
John D <az0006t@protonmail.com>
2025-02-19 19:25:18 -0500

    updates

--- src/network/network_init.h
+++ src/network/network_init.h
@@ -4,11 +4,6 @@
 #include <stdbool.h>
 #include <netinet/in.h>  // For sockaddr_in and sockaddr_in6
 
-// Global Variables
-extern int server_socket;
-extern int epoll_fd;
-
-// Function Prototypes
 bool create_server(void);
 
 //void start_epoll_event_loop(void);
