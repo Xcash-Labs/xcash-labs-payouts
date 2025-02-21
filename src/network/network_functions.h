5e709c147eaf81ee76f8178231ee0f22f195bab0
John D <az0006t@protonmail.com>
2025-02-20 19:34:23 -0500

    Update network_functions.h

--- src/network/network_functions.h
+++ src/network/network_functions.h
@@ -8,6 +8,7 @@
 #include <ctype.h>
 #include <arpa/inet.h>
 #include "config.h"
+#include "globals.h"
 
 int check_if_IP_address_or_hostname(const char* HOST);
 size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp);
@@ -15,4 +16,4 @@ int send_http_request(char *result, const char *host, const char *url, int port,
     const char *method, const char **headers, size_t headers_length, 
     const char *data, int timeout);
 
-    #endif
+#endif
