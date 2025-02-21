2df8dbda0f201cf9fec99a966cf6517a05f0ed1f
John D <az0006t@protonmail.com>
2025-02-20 19:34:50 -0500

    Update network_functions.h

--- src/network/network_functions.h
+++ src/network/network_functions.h
@@ -16,4 +16,5 @@ int send_http_request(char *result, const char *host, const char *url, int port,
     const char *method, const char **headers, size_t headers_length, 
     const char *data, int timeout);
 
+    
 #endif
