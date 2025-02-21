07d8a3db50704dedaee68e347f383c86d0af0a69
John D <az0006t@protonmail.com>
2025-02-21 12:15:27 -0500

    Update dpops_round.c

--- src/dpops_round/dpops_round.c
+++ src/dpops_round/dpops_round.c
@@ -34,12 +34,10 @@ void start_block_production(void)
                 round_result = ROUND_OK;
             }
             else
-            }
+            {
                 INFO_STAGE_PRINT("Waiting for a [%d], block production. Starting in ... [%lu:%02lu]",
                     (int) atof(current_block_height), BLOCK_TIME - 1 - minute_within_block, 59 - (current_time.tv_sec % 60));
-
-
-                                 sleep(5);
+                sleep(5);
             }
         }
         else
