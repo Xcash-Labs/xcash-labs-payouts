a94d980cc0a338157e8516ebf9f8de1656818a77
John D <az0006t@protonmail.com>
2025-02-20 19:22:10 -0500

    updates

--- src/network/network_functions.h
+++ src/network/network_functions.h
@@ -1,16 +1,18 @@
 #ifndef NETWORK_FUNCTIONS_H_   /* Include guard */
 #define NETWORK_FUNCTIONS_H_
 
-/*
------------------------------------------------------------------------------------------------------------
-Function prototypes
------------------------------------------------------------------------------------------------------------
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <curl/curl.h>
+#include <ctype.h>
+#include <arpa/inet.h>
+#include "config.h"
 
 int check_if_IP_address_or_hostname(const char* HOST);
-int send_http_request(char *result, const char* HOST, const char* URL, const int PORT, const char* HTTP_SETTINGS, const char* HTTP_HEADERS[], const size_t HTTP_HEADERS_LENGTH, const char* DATA, const int DATA_TIMEOUT_SETTINGS);
-int send_and_receive_data_socket(char *result, const size_t RESULT_LENGTH, const char* HOST, const int PORT, const char* DATA, const int DATA_TIMEOUT_SETTINGS);
-int send_data_socket(const char* HOST, const int PORT, const char* DATA, const int DATA_TIMEOUT_SETTINGS);
-int send_data(const int SOCKET, unsigned char* data, const long DATA_LENGTH, const int MESSAGE_SETTINGS, const char* MESSAGE_DATA_SETTINGS);
-int receive_data(const int SOCKET, char *message, const size_t LENGTH, const int RECEIVE_DATA_SOCKET_TIMEOUT_SETTINGS, const int RECEIVE_DATA_SOCKET_TIMEOUT);
-#endif
+size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp);
+int send_http_request(char *result, const char *host, const char *url, int port, 
+    const char *method, const char **headers, size_t headers_length, 
+    const char *data, int timeout);
+
+    #endif
