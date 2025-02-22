ead309c142f3fb4154069cdd5b78de761dcc9b88
John D <az0006t@protonmail.com>
2025-02-22 10:04:50 -0500

    Update cached_hashes.h

--- src/db/cashed_hashed/cached_hashes.h
+++ src/db/cashed_hashed/cached_hashes.h
@@ -4,11 +4,11 @@
 #include <bson/bson.h>
 #include <mongoc/mongoc.h>
 #include <stdio.h>
-#include "md5.h"
+//#include "md5.h"
 #include "config.h"
 #include "globals.h"
 #include "macro_functions.h"
-#include <time.h>
+//#include <time.h>
 
 int del_hash(mongoc_client_t *client, const char *db_name);
 
