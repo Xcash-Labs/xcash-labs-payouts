24c7651b526fa772cb7b9d51bad7dfefa1a4beff
John D <az0006t@protonmail.com>
2025-02-22 09:48:51 -0500

    updates

--- src/db/cashed_hashed/cached_hashes.h
+++ src/db/cashed_hashed/cached_hashes.h
@@ -5,11 +5,10 @@
 #include <mongoc/mongoc.h>
 #include <stdio.h>
 #include "md5.h"
-//#include "define_macro_functions.h"
+#include "config.h"
+#include "macro_functions.h"
 #include <time.h>
 
-int get_multi_hash(mongoc_client_t *client, const char *db_prefix, char *hash);
 int del_hash(mongoc_client_t *client, const char *db_name);
-int drop_all_hashes(mongoc_client_t *client);
 
 #endif
