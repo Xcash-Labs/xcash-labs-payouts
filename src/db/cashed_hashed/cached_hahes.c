996605b17374ea21b5269ca75339cd5bf272ebe5
John D <az0006t@protonmail.com>
2025-02-22 09:54:01 -0500

    Update cached_hahes.c

--- src/db/cashed_hashed/cached_hahes.c
+++ src/db/cashed_hashed/cached_hahes.c
@@ -1,30 +1,32 @@
 # include "cached_hashes.h"
 
-
 /*---------------------------------------------------------------------------------------------------------
-Name: insert_document_into_collection_json
-Description: Inserts a document into the collection in the database from json data
+Name: del_hash
+Description: Deletes a hash entry from the "hashes" collection in the specified database.
+             If the collection does not exist, the function returns 0 without attempting deletion.
 Parameters:
-  DATABASE - The database name
-  COLLECTION - The collection name
-  DATA - The json data to insert into the collection
-Return: 0 if an error has occured, 1 if successfull
+  client   - A pointer to the MongoDB client connection.
+  db_name  - The name of the database where the "hashes" collection is located.
+Return: 
+  -1 if an error occurs (e.g., database/collection access failure, deletion error).
+   0 if the collection does not exist or no matching document was found.
+   1 if a hash was successfully deleted.
 ---------------------------------------------------------------------------------------------------------*/
 int del_hash(mongoc_client_t *client, const char *db_name) {
     if (!client || !db_name) {
-        PRINT_ERROR("del_hash() received NULL parameters.\n");
+        ERROR_PRINT("del_hash() received NULL parameters.");
         return -1;
     }
 
     mongoc_collection_t *collection = mongoc_client_get_collection(client, db_name, "hashes");
     if (!collection) {
-        PRINT_ERROR("Failed to get collection 'hashes' for database: %s\n", db_name);
+        ERROR_PRINT("Failed to get collection 'hashes' for database: %s", db_name);
         return -1;
     }
 
     bson_t *filter = BCON_NEW("db_name", BCON_UTF8(db_name));
     if (!filter) {
-        PRINT_ERROR("Failed to create BSON filter for db_name: %s\n", db_name);
+        ERROR_PRINT("Failed to create BSON filter for db_name: %s", db_name);
         mongoc_collection_destroy(collection);
         return -1;
     }
@@ -33,10 +35,10 @@ int del_hash(mongoc_client_t *client, const char *db_name) {
     int result = 0;
 
     if (mongoc_collection_delete_one(collection, filter, NULL, NULL, &error)) {
-        PRINT_DEBUG("Hash successfully deleted for %s\n", db_name);
+        DEBUG_PRINT("Hash successfully deleted for %s", db_name);
         result = XCASH_OK;
     } else {
-        PRINT_ERROR("Delete hashes %s failed: %s\n", db_name, error.message);
+        ERROR_PRINT("Delete hashes %s failed: %s", db_name, error.message);
         result = -1;
     }
 
