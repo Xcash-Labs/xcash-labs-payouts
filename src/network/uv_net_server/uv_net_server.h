21968ca24ade68b2cc97638c2150ead5cef5307e
John D <az0006t@protonmail.com>
2025-02-22 22:19:36 -0500

    Update uv_net_server.h

--- src/network/uv_net_server/uv_net_server.h
+++ src/network/uv_net_server/uv_net_server.h
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <uv.h>
 #include "config.h"
-
+#include "macro_functions.h"
 
 void alloc_buffer(uv_handle_t *handle, size_t suggested_size, uv_buf_t *buf);
 void on_client_read(uv_stream_t *client, ssize_t nread, const uv_buf_t *buf);
