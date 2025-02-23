3e00fdb6d312e76d0a428e76a4c3f35823181413
John D <az0006t@protonmail.com>
2025-02-23 16:55:49 -0500

    Update macro_functions.h

--- src/core/macro_functions.h
+++ src/core/macro_functions.h
@@ -17,8 +17,8 @@
 #define __DEBUG_PRINT_FUNC_CALLER if (debug_enabled)fprintf(stderr, "  --> TRACE: %s:%d, %s()\n\n", __FILE__, __LINE__, __func__);
 #define FATAL_ERROR_EXIT(fmt, ...) do { fprintf(stderr, "\033[1;31mFATAL: " fmt "\033[0m\n", ##__VA_ARGS__); __DEBUG_PRINT_FUNC_CALLER; exit(1); } while (0)
 #define DEBUG_PRINT(fmt, ...) do { if (debug_enabled) fprintf(stderr, "\033[1;35mDEBUG: " fmt "\033[0m\n", ##__VA_ARGS__); __DEBUG_PRINT_FUNC_CALLER; } while (0)
-#define INFO_PRINT(fmt, ...) do { fprintf(stderr, BRIGHT_WHITE_TEXT("INFO: ") fmt "\n\n", ##__VA_ARGS__); __DEBUG_PRINT_FUNC_CALLER; } while (0)
-#define INFO_STAGE_PRINT(fmt, ...) do { fprintf(stderr, BRIGHT_WHITE_TEXT("\n\nINFO: ") LIGHT_BLUE_TEXT(fmt) "\n\n", ##__VA_ARGS__); __DEBUG_PRINT_FUNC_CALLER; } while (0)
+#define INFO_PRINT(fmt, ...) do { fprintf(stderr, BRIGHT_WHITE_TEXT("INFO: ") fmt "\n", ##__VA_ARGS__); __DEBUG_PRINT_FUNC_CALLER; } while (0)
+#define INFO_STAGE_PRINT(fmt, ...) do { fprintf(stderr, BRIGHT_WHITE_TEXT("\n\nINFO: ") LIGHT_BLUE_TEXT(fmt) "\n", ##__VA_ARGS__); __DEBUG_PRINT_FUNC_CALLER; } while (0)
 #define WARNING_PRINT(fmt, ...) do { fprintf(stderr, ORANGE_TEXT("WARNING: ") fmt "\n", ##__VA_ARGS__); __DEBUG_PRINT_FUNC_CALLER; } while (0)
 #define ERROR_PRINT(fmt, ...) do { fprintf(stderr, RED_TEXT("ERROR: ") fmt "\n", ##__VA_ARGS__); __DEBUG_PRINT_FUNC_CALLER; } while (0)
 #define INFO_PRINT_STATUS_OK(fmt, ...) do { fprintf(stderr, BRIGHT_WHITE_TEXT("INFO: ") fmt INFO_STATUS_OK "\n", ##__VA_ARGS__); __DEBUG_PRINT_FUNC_CALLER; } while (0)
