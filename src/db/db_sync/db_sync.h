38ae9acefad4d1c5a96cd5f1610759d4f04c7a8b
John D <az0006t@protonmail.com>
2025-02-24 21:44:50 -0500

    Update db_sync.h

--- src/db/db_sync/db_sync.h
+++ src/db/db_sync/db_sync.h
@@ -7,4 +7,11 @@
 #include "globals.h"
 #include "macro_functions.h"
 
+typedef struct {
+    size_t block_height; // the block height
+    bool db_reserve_bytes_synced;
+    char public_address[XCASH_WALLET_LENGTH+1];
+    char db_hashes[DATABASE_TOTAL][DATA_HASH_LENGTH+1];
+} xcash_node_sync_info_t;
+
 #endif
