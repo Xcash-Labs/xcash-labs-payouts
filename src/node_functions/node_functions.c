923f57661f3c8c283b95d0c4df9633a21cb4425f
John D <az0006t@protonmail.com>
2025-02-26 22:04:51 -0500

    Update node_functions.c

--- src/node_functions/node_functions.c
+++ src/node_functions/node_functions.c
@@ -4,12 +4,12 @@ bool get_node_data(void) {
     // Get the wallet's public address
     if (!get_public_address()) {
         DEBUG_PRINT("Could not get the wallet's public address");
-        return XCASH_ERROR
+        return XCASH_ERROR;
     }
 
     if (xcash_wallet_public_address[0] == '\0') {
         DEBUG_PRINT("Wallet public address is empty");
-        return XCASH_ERROR
+        return XCASH_ERROR;
     }
 
     is_seed_node = is_seed_address(xcash_wallet_public_address);
@@ -24,7 +24,7 @@ int is_seed_address(const char* public_address) {
     }
     for (size_t i = 0; network_nodes.seed_public_address[i] != NULL; i++) {
         if (strcmp(network_nodes.seed_public_address[i], public_address) == 0) {
-            return true
+            return true;
         }
     }
     return false;
