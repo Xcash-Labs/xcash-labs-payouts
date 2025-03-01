e0cc46322abebfc5fc46c458c3b4d4f2a99271da
John D <az0006t@protonmail.com>
2025-02-28 22:06:41 -0500

    Update xcash_net.h

--- src/network/xcash_net/xcash_net.h
+++ src/network/xcash_net/xcash_net.h
@@ -17,32 +17,16 @@ typedef enum XNET_DEST{
     XNET_SEEDS_ALL_ONLINE,
     XNET_DELEGATES_ALL,
     XNET_DELEGATES_ALL_ONLINE,
-
     XNET_NONE
 } xcash_dest_t;
 
-// Remove trailing "|END|" from each valid response in the array.
 void remove_enders(response_t **responses);
-
-// Sends a message (appending "|END|") to a predefined group of nodes.
 bool xnet_send_data_multi(xcash_dest_t dest, const char* message, response_t ***reply);
-
-// Sends a message with parameter list to a group of nodes.
 bool send_message_param_list(xcash_dest_t dest, xcash_msg_t msg, response_t ***reply, const char** pair_params);
-
-// Sends a message with variadic parameters to a group of nodes.
 bool send_message_param(xcash_dest_t dest, xcash_msg_t msg, response_t ***reply, ...);
-
-// Sends a basic message with no parameters.
 bool send_message(xcash_dest_t dest, xcash_msg_t msg, response_t ***reply);
-
-// Sends a message with parameter list directly to a single host.
 bool send_direct_message_param_list(const char* host, xcash_msg_t msg, response_t ***reply, const char** pair_params);
-
-// Sends a message with variadic parameters directly to a single host.
 bool send_direct_message_param(const char* host, xcash_msg_t msg, response_t ***reply, ...);
-
-// Sends a basic message with no parameters to a single host.
 bool send_direct_message(const char* host, xcash_msg_t msg, response_t ***reply);
 
 #endif // XCASH_NET_H
