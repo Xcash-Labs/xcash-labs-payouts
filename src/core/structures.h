79117ce25d7f58f2c32a86129cb73e81d2d52f2c
John D <az0006t@protonmail.com>
2025-02-28 22:34:57 -0500

    Update structures.h

--- src/core/structures.h
+++ src/core/structures.h
@@ -7,4 +7,26 @@ typedef struct {
     const char *ip_address;
 } NetworkNode;
 
+// xcash-next
+typedef struct  {
+    char public_address[XCASH_WALLET_LENGTH+1];
+    char total_vote_count[100];
+    char IP_address[256];
+    char delegate_name[MAXIMUM_BUFFER_SIZE_DELEGATES_NAME+1];
+    char about[1025];
+    char website[256];
+    char team[256];
+    char shared_delegate_status[10];
+    char delegate_fee[11];
+    char server_specs[1025];
+    char block_verifier_score[10];
+    char online_status[10];
+    char block_verifier_total_rounds[10];
+    char block_verifier_online_total_rounds[10];
+    char block_verifier_online_percentage[10];
+    char block_producer_total_rounds[10];
+    char block_producer_block_heights[BUFFER_SIZE_BLOCK_HEIGHTS_DATA];
+    char public_key[VRF_PUBLIC_KEY_LENGTH+1];
+} delegates_t; 
+
 #endif
