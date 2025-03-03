900bed2d1a473566446ec40fb1a98ccac7a43fd3
John D <az0006t@protonmail.com>
2025-03-03 16:29:22 -0500

    Update config.h

--- src/core/config.h
+++ src/core/config.h
@@ -15,8 +15,6 @@
 #define XCASH_DPOPS_IP "127.0.0.1" // The X-CASH Wallet IP
 #define SOCKET_END_STRING "|END|" // End string when sending data between nodes, to signal the end of sending data
 
-#define DATABASE_CONNECTION "mongodb://127.0.0.1:27017" // The database connection string
-
 #define XCASH_WALLET_PREFIX "XCA" // The prefix of a XCA address
 #define XCASH_SIGN_DATA_PREFIX "SigV1" // The prefix of a XCASH_DPOPS_signature for the signed data
 
@@ -28,12 +26,14 @@
 #define MAX_CONNECTIONS 1024 // Max connection for incomming transactions
 
 // Database
+#define DATABASE_CONNECTION "mongodb://127.0.0.1:27017" // The database connection string
 #define DATABASE_NAME "XCASH_PROOF_OF_STAKE" // The name of the database
 #define DATABASE_NAME_DELEGATES "XCASH_PROOF_OF_STAKE_DELEGATES" // The name of the database for the delegates
 #define MAXIMUM_DATABASE_WRITE_SIZE 48000000 // The maximum database write size
 #define DATABASE_TOTAL 4 // The amount of databases
 #define TOTAL_DELEGATES_DATABASE_FIELDS 18 // The total delegates database fields
 #define MAXIMUM_DATABASE_COLLECTION_DOCUMENTS 5000 // The maximum amount of documents in a database collection
+#define DATABASE_EMPTY_STRING "empty_database_collection" // The database data to give when the database collection is empty
 
 // Lengths
 #define IP_LENGTH 253
@@ -49,7 +49,7 @@
 #define RANDOM_STRING_LENGTH 100 // The length of the random string
 #define XCASH_SIGN_DATA_LENGTH 93 // The length of a XCASH_DPOPS_signature for the signed data
 
-#define TOTAL_ERROR_MESSAGES 1000 // The total amount of error messages              ????????????????????????????
+//#define TOTAL_ERROR_MESSAGES 1000 // The total amount of error messages              ????????????????????????????
 
 #define BLOCK_VERIFIERS_IP_ADDRESS_TOTAL_LENGTH 100 // The maximum length of the block verifiers IP address  ????
 #define VRF_SECRET_KEY_LENGTH 128 // Length of VRF Secret Key
