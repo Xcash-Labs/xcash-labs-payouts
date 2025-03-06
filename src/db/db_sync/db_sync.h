70fe73a52bb429abe2b4e36d7e24dd7341c0feec
John D <az0006t@protonmail.com>
2025-03-06 12:53:23 -0500

    Updates

--- src/db/db_sync/db_sync.h
+++ src/db/db_sync/db_sync.h
@@ -10,6 +10,7 @@
 #include "xcash_net.h"
 #include "db_helpers.h"
 #include "db_operations.h"
+#include "node_functions.h"
 
 typedef struct {
     size_t block_height; // the block height
@@ -28,8 +29,33 @@ typedef struct
 void show_majority_statistics(const xcash_node_sync_info_t* majority_list, size_t items_count);
 bool get_sync_nodes_majority_list_top(xcash_node_sync_info_t** majority_list_result, size_t* majority_count_result);
 bool initial_db_sync_check(size_t* majority_count, xcash_node_sync_info_t** majority_list_result);
-
 bool check_sync_nodes_majority_list(response_t** replies, xcash_node_sync_info_t** majority_list_result,
     size_t* majority_count_result, bool by_top_block_height);
+
+    bool get_node_sync_info(xcash_node_sync_info_t* sync_info);
+    bool download_db_from_node(const char* host, xcash_dbs_t db_type, int index, char* result_db_data_buf,
+                               size_t result_db_data_buf_size);
+    void cleanup_db_sync_results(xcash_db_sync_obj_t** sync_objs_result);
+    size_t get_db_sub_count(xcash_dbs_t db_type);
+    bool send_db_sync_request_to_all_seeds(xcash_dbs_t db_type, size_t start_db_index, response_t*** reply);
+    bool check_multi_db_hashes_from_seeds(xcash_dbs_t db_type, xcash_db_sync_obj_t*** sync_objs_result);
+    bool parse_nodes_sync_reply(response_t** replies, xcash_dbs_t db_type, xcash_db_sync_obj_t*** sync_objs_result);
+    bool check_db_hashes_from_seeds(xcash_dbs_t db_type, xcash_db_sync_obj_t*** sync_objs_result);
+    bool update_db_from_node(const char* public_address, xcash_dbs_t db_type);
+    bool fill_delegates_from_db(void);
+    bool get_actual_nodes_list(bool is_seeds_offline);
+    xcash_node_sync_info_t** make_nodes_majority_list(xcash_node_sync_info_t* sync_states_list, size_t states_count,
+                                                      bool by_top_block_height);
+    bool get_sync_seeds_majority_list(xcash_node_sync_info_t** majority_list_result, size_t* majority_count_result);
+    bool get_sync_nodes_majority_list(xcash_node_sync_info_t** majority_list_result, size_t* majority_count_result);
+    bool check_sync_nodes_majority_list(response_t** replies, xcash_node_sync_info_t** majority_list_result,
+                                        size_t* majority_count_result, bool by_top_block_height);
+    int get_random_majority(xcash_node_sync_info_t* majority_list, size_t majority_count);
+    bool initial_db_sync_check(size_t* majority_count, xcash_node_sync_info_t** majority_list_result);
+    bool check_time_sync_to_seeds(void);
+    bool init_db_from_seeds(void);
+    bool init_db_from_top(void);
+    bool synchronize_database_from_specific_delegate(const char* delegate_ip);
+    bool synchronize_database_from_network_data_node(void);
 
 #endif
