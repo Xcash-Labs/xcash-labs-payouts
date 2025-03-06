611fcf4de2b81eaa8ed39c8f3e7137d202e1dbe1
John D <az0006t@protonmail.com>
2025-03-05 21:22:36 -0500

    Updates

--- src/functions/VRF_functions/VRF_functions.c
+++ src/functions/VRF_functions/VRF_functions.c
@@ -1,13 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/random.h>
-
-#include "macro_functions.h"
-#include "config.h"
-#include "convert.h"
-#include "vrf.h"
-#include "crypto_vrf.h"
 #include "VRF_functions.h"
 
 /*---------------------------------------------------------------------------------------------------------
--- src/functions/VRF_functions/VRF_functions.h
+++ src/functions/VRF_functions/VRF_functions.h
@@ -1,6 +1,17 @@
 #ifndef VRF_FUNCTIONS_H_   /* Include guard */
 #define VRF_FUNCTIONS_H_
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/random.h>
+#include "config.h"
+#include "globals.h"
+#include "macro_functions.h"
+#include "convert.h"
+#include "vrf.h"
+#include "crypto_vrf.h"
+
 void generate_key(void);
 int VRF_sign_data(char *beta_string, char *proof, const char* data);
 
