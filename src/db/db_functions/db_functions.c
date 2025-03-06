b37922ea8ff3b661c9ae811607781a3db5cbe5fc
John D <az0006t@protonmail.com>
2025-03-06 10:39:06 -0500

    Update db_functions.c

--- src/db/db_functions/db_functions.c
+++ src/db/db_functions/db_functions.c
@@ -710,7 +710,7 @@ int get_database_data_hash(char *data_hash, const char *DATABASE, const char *CO
           snprintf(data + strlen(data), sizeof(data) - strlen(data), "reserve_bytes_%zu", count);
           if (count != count2) strncat(data, "\",\"", sizeof(data) - strlen(data) - 1);
         } 
-      } else {}
+      } else {        return XCASH_ERROR; }
     } else if (strncmp(COLLECTION, "reserve_proofs", BUFFER_SIZE) == 0) {
         for (count = 1; count <= TOTAL_RESERVE_PROOFS_DATABASES; ++count) {
             snprintf(data + strlen(data), sizeof(data) - strlen(data), "reserve_proofs_%zu", count);
