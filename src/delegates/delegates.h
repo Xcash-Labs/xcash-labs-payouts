fda76c59513aa07cd5f10cf4b72ffacf832c1e21
John D <az0006t@protonmail.com>
2025-03-06 14:20:50 -0500

    Update delegates.h

--- src/delegates/delegates.h
+++ src/delegates/delegates.h
@@ -2,7 +2,7 @@
 #define __XCASH_DELEGATES_H
 
 #include "db_operations.h"
-#include "xcash_db_helpers.h"
+#include "db_helpers.h"
 #include "globals.h"
 
 int read_organize_delegates(delegates_t* delegates, size_t* delegates_count_result);
