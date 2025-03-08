aa83bb66db1ed7be9cad111fa8fb36b0badb5a7c
John D <az0006t@protonmail.com>
2025-03-07 22:05:01 -0500

    Update network_daemon_functions.c

--- src/network/network_daemon_functions/network_daemon_functions.c
+++ src/network/network_daemon_functions/network_daemon_functions.c
@@ -21,23 +21,9 @@ int get_current_block_height(char *result)
     if (send_http_request(response_data, XCASH_DAEMON_IP, "/json_rpc", XCASH_DAEMON_PORT,
                           "POST", HTTP_HEADERS, HTTP_HEADERS_LENGTH, request_payload,
                           SEND_OR_RECEIVE_SOCKET_DATA_TIMEOUT_SETTINGS) != XCASH_OK ||
-        parse_json_data(response_data, "result.count", result, SMALL_BUFFER_SIZE) == 0)
-    {
-        memset(response_data, 0, sizeof(response_data));
-        memset(result, 0, SMALL_BUFFER_SIZE);
-        sleep(INVALID_RESERVE_PROOFS_SETTINGS);
-
-        // Retry if the first attempt failed
-        if (send_http_request(response_data, XCASH_DAEMON_IP, "/json_rpc", XCASH_DAEMON_PORT,
-                              "POST", HTTP_HEADERS, HTTP_HEADERS_LENGTH, request_payload,
-                              SEND_OR_RECEIVE_SOCKET_DATA_TIMEOUT_SETTINGS) != XCASH_OK ||
-            parse_json_data(response_data, "result.count", result, SMALL_BUFFER_SIZE) == 0)
-        {
-            ERROR_PRINT("Could not get the current block height");
-            return XCASH_ERROR;
-        }
+        parse_json_data(response_data, "result.count", result, SMALL_BUFFER_SIZE) == 0) {
+        return XCASH_ERROR;
     }
-
     return XCASH_OK;
 }
 
