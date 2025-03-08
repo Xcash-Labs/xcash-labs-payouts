ba57a8acf40d0227c0672824c97c682774d4676d
John D <az0006t@protonmail.com>
2025-03-08 18:25:15 -0500

    Update xcash_dpops.c

--- src/xcash_dpops.c
+++ src/xcash_dpops.c
@@ -183,7 +183,7 @@ int main(int argc, char *argv[]) {
     FATAL_ERROR_EXIT("Failed to convert the block-verifiers-secret-key to a byte array: %s", arg_config.block_verifiers_secret_key);
   }
 
-  if (!configure_uv_threadpool(arg_config)) {
+  if (!configure_uv_threadpool(&arg_config)) {
       FATAL_ERROR_EXIT("Failed to set UV_THREADPOOL_SIZE.");
   }
 
