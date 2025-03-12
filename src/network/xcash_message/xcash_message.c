a4a1c0cd3f36b67aa9d2eafd6f4c885ba007fc76
John D <az0006t@protonmail.com>
2025-03-12 12:15:02 -0400

    Update xcash_message.c

--- src/network/xcash_message/xcash_message.c
+++ src/network/xcash_message/xcash_message.c
@@ -430,38 +430,38 @@ void handle_srv_message(const char* data, size_t length, client_t* client) {
   xcash_msg_t msg_type = get_message_type(data);
   switch (msg_type) {
     case XMSG_XCASH_GET_BLOCK_HASH:
-      if (server_limit_IP_addresses(1, client_ip) == 1) {
+      if (server_limit_IP_addresses(1, client->client_ip) == 1) {
         server_received_msg_get_block_hash(client, data);
-        server_limit_IP_addresses(0, client_ip);
+        server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
     case XMSG_XCASH_GET_SYNC_INFO:
-      if (server_limit_IP_addresses(1, client_ip) == 1) {
+      if (server_limit_IP_addresses(1, client->client_ip) == 1) {
         server_received_msg_get_sync_info(client, data);
-        server_limit_IP_addresses(0, client_ip);
+        server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
     case XMSG_XCASH_GET_BLOCK_PRODUCERS:
-      if (server_limit_IP_addresses(1, client_ip) == 1) {
+      if (server_limit_IP_addresses(1, client->client_ip) == 1) {
         server_received_msg_get_block_producers(client, data);
-        server_limit_IP_addresses(0, client_ip);
+        server_limit_IP_addresses(0, client->client_ip);
       }
       break;
     case XMSG_GET_CURRENT_BLOCK_HEIGHT:
-      if (server_limit_IP_addresses(1, client_ip) == 1) {
-        server_receive_data_socket_get_current_block_height(client_ip);
-        server_limit_IP_addresses(0, client_ip);
+      if (server_limit_IP_addresses(1, client->client_ip) == 1) {
+        server_receive_data_socket_get_current_block_height(client->client_ip);
+        server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
     case XMSG_SEND_CURRENT_BLOCK_HEIGHT:
-      if (server_limit_IP_addresses(1, client_ip) == 1) {
+      if (server_limit_IP_addresses(1, client->client_ip) == 1) {
         pthread_mutex_lock(&update_current_block_height_lock);
         server_receive_data_socket_send_current_block_height(data);
         pthread_mutex_unlock(&update_current_block_height_lock);
-        server_limit_IP_addresses(0, client_ip);
+        server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
@@ -476,33 +476,33 @@ void handle_srv_message(const char* data, size_t length, client_t* client) {
 
     case XMSG_NODE_TO_NETWORK_DATA_NODES_GET_CURRENT_BLOCK_VERIFIERS_LIST:
       if ((strstr(data, "\"public_address\"") != NULL && server_limit_public_addresses(1, data) == 1) ||
-          (strstr(data, "\"public_address\"") == NULL && server_limit_IP_addresses(1, client_ip) == 1)) {
+          (strstr(data, "\"public_address\"") == NULL && server_limit_IP_addresses(1, client->client_ip) == 1)) {
         server_receive_data_socket_node_to_network_data_nodes_get_current_block_verifiers_list(client);
         strstr(data, "\"public_address\"") != NULL ? server_limit_public_addresses(3, data)
-                                                   : server_limit_IP_addresses(0, client_ip);
+                                                   : server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
     case XMSG_NETWORK_DATA_NODES_TO_NETWORK_DATA_NODES_DATABASE_SYNC_CHECK:
-      if (server_limit_IP_addresses(1, client_ip) == 1) {
+      if (server_limit_IP_addresses(1, client->client_ip) == 1) {
         server_receive_data_socket_network_data_nodes_to_network_data_nodes_database_sync_check(data);
-        server_limit_IP_addresses(0, client_ip);
+        server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
     case XMSG_NODE_TO_BLOCK_VERIFIERS_GET_RESERVE_BYTES_DATABASE_HASH:
       if ((strstr(data, "|") != NULL && server_limit_public_addresses(2, data) == 1) ||
-          (strstr(data, "|") == NULL && server_limit_IP_addresses(1, client_ip) == 1)) {
+          (strstr(data, "|") == NULL && server_limit_IP_addresses(1, client->client_ip) == 1)) {
         server_receive_data_socket_node_to_block_verifiers_get_reserve_bytes_database_hash(client, data);
         strstr(data, "|") != NULL ? server_limit_public_addresses(4, data)
-                                  : server_limit_IP_addresses(0, client_ip);
+                                  : server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
     case XMSG_NODE_TO_BLOCK_VERIFIERS_CHECK_IF_CURRENT_BLOCK_VERIFIER:
-      if (server_limit_IP_addresses(1, client_ip) == 1) {
+      if (server_limit_IP_addresses(1, client->client_ip) == 1) {
         server_receive_data_socket_node_to_block_verifiers_check_if_current_block_verifier(client);
-        server_limit_IP_addresses(0, client_ip);
+        server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
@@ -563,11 +563,11 @@ void handle_srv_message(const char* data, size_t length, client_t* client) {
       break;
 
     case XMSG_NODE_TO_BLOCK_VERIFIERS_ADD_RESERVE_PROOF:
-      if (server_limit_IP_addresses(1, client_ip) == 1) {
+      if (server_limit_IP_addresses(1, client->client_ip) == 1) {
         pthread_mutex_lock(&add_reserve_proof_lock);
         server_receive_data_socket_node_to_block_verifiers_add_reserve_proof(client, data);
         pthread_mutex_unlock(&add_reserve_proof_lock);
-        server_limit_IP_addresses(0, client_ip);
+        server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
@@ -581,23 +581,23 @@ void handle_srv_message(const char* data, size_t length, client_t* client) {
       break;
 
     case XMSG_NODES_TO_BLOCK_VERIFIERS_REGISTER_DELEGATE:
-      if (server_limit_IP_addresses(1, client_ip) == 1) {
+      if (server_limit_IP_addresses(1, client->client_ip) == 1) {
         server_receive_data_socket_nodes_to_block_verifiers_register_delegates(client, data);
-        server_limit_IP_addresses(0, client_ip);
+        server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
     case XMSG_NODES_TO_BLOCK_VERIFIERS_UPDATE_DELEGATE:
-      if (server_limit_IP_addresses(1, client_ip) == 1) {
+      if (server_limit_IP_addresses(1, client->client_ip) == 1) {
         server_receive_data_socket_nodes_to_block_verifiers_update_delegates(client, data);
-        server_limit_IP_addresses(0, client_ip);
+        server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
     case XMSG_NODE_TO_NETWORK_DATA_NODES_CHECK_VOTE_STATUS:
-      if (server_limit_IP_addresses(1, client_ip) == 1) {
+      if (server_limit_IP_addresses(1, client->client_ip) == 1) {
         server_receive_data_socket_nodes_to_network_data_nodes_check_vote_status(client, data);
-        server_limit_IP_addresses(0, client_ip);
+        server_limit_IP_addresses(0, client->client_ip);
       }
       break;
 
