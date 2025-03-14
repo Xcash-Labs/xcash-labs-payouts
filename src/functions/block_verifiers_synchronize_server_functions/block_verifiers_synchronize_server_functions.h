3a4f444d67b8ee9c12205295deffcd6095e762bf
John D <az0006t@protonmail.com>
2025-03-13 21:14:20 -0400

    Updates

---
+++ src/functions/block_verifiers_synchronize_server_functions/block_verifiers_synchronize_server_functions.h
@@ -0,0 +1,33 @@
+#ifndef BLOCK_VERIFIERS_SYNCHRONIZE_SERVER_FUNCTIONS_H_   /* Include guard */
+#define BLOCK_VERIFIERS_SYNCHRONIZE_SERVER_FUNCTIONS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "config.h"
+#include "globals.h"
+#include "macro_functions.h"
+#include "uv_net_server.h"
+
+void server_received_msg_get_block_hash(const int CLIENT_SOCKET, const char* MESSAGE);
+
+void server_received_msg_get_block_producers(const int CLIENT_SOCKET, const char* MESSAGE);
+
+void server_received_msg_get_sync_info(const int CLIENT_SOCKET, const char* MESSAGE);
+
+void server_receive_data_socket_get_current_block_height(const char* CLIENT_IP_ADDRESS);
+void server_receive_data_socket_send_current_block_height(const char* MESSAGE);
+void server_receive_data_socket_node_to_network_data_nodes_get_previous_current_next_block_verifiers_list(const int CLIENT_SOCKET);
+void server_receive_data_socket_node_to_network_data_nodes_get_current_block_verifiers_list(const int CLIENT_SOCKET);
+void server_receive_data_socket_network_data_nodes_to_network_data_nodes_database_sync_check(const char* MESSAGE);
+void server_receive_data_socket_node_to_block_verifiers_get_reserve_bytes_database_hash(const int CLIENT_SOCKET, const char* MESSAGE);
+void server_receive_data_socket_node_to_block_verifiers_check_if_current_block_verifier(const int CLIENT_SOCKET);
+void server_receive_data_socket_block_verifiers_to_block_verifiers_reserve_proofs_database_sync_check_all_update(const int CLIENT_SOCKET, const char* MESSAGE);
+void server_receive_data_socket_block_verifiers_to_block_verifiers_reserve_proofs_database_download_file_update(const int CLIENT_SOCKET, const char* MESSAGE);
+void server_receive_data_socket_block_verifiers_to_block_verifiers_reserve_bytes_database_sync_check_all_update(const int CLIENT_SOCKET, const char* MESSAGE);
+void server_receive_data_socket_block_verifiers_to_block_verifiers_reserve_bytes_database_download_file_update(const int CLIENT_SOCKET, const char* MESSAGE);
+void server_receive_data_socket_block_verifiers_to_block_verifiers_delegates_database_sync_check_update(const int CLIENT_SOCKET, const char* MESSAGE);
+void server_receive_data_socket_block_verifiers_to_block_verifiers_delegates_database_download_file_update(const int CLIENT_SOCKET);
+void server_receive_data_socket_block_verifiers_to_block_verifiers_statistics_database_sync_check_update(const int CLIENT_SOCKET, const char* MESSAGE);
+void server_receive_data_socket_block_verifiers_to_block_verifiers_statistics_database_download_file_update(const int CLIENT_SOCKET);
+#endif
