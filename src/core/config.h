74a8197ff3142612b3f5e6f57a8b764a8e241974
John D <az0006t@protonmail.com>
2025-03-22 19:30:29 -0400

    Update config.h

--- src/core/config.h
+++ src/core/config.h
@@ -3,10 +3,10 @@
 
 #include <stdbool.h>
 
-// XCASH Version
+// ===================== XCASH Version =====================
 #define XCASH_DPOPS_CURRENT_VERSION "xCash Labs DPoPs V. 2.0.0"
 
-// Blockchain Settings
+// ===================== Blockchain Settings =====================
 #define XCASH_WALLET_PREFIX "XCA"
 #define XCASH_TOTAL_SUPPLY 100000000000
 #define XCASH_PREMINE_TOTAL_SUPPLY 40000000000
@@ -17,7 +17,7 @@
 #define XCASH_PROOF_OF_STAKE_BLOCK_HEIGHT 2
 #define UNLOCK_BLOCK_AMOUNT 60
 
-// Blockchain Length and Data Definitions
+// ===================== Blockchain Data Lengths =====================
 #define BLOCKCHAIN_RESERVED_BYTES_LENGTH_TEXT "7c424c4f434b434841494e5f52455345525645445f42595445535f4c454e4754487c"
 #define BLOCKCHAIN_EXTRA_BYTES_LENGTH_TEXT "7c424c4f434b434841494e5f45585452415f42595445535f4c454e4754487c"
 #define BLOCKCHAIN_DATA_SEGMENT_STRING "7c424c4f434b434841494e5f444154415f5345474d454e545f535452494e477c"
@@ -26,7 +26,7 @@
 #define BLOCKCHAIN_RESERVED_BYTES_END "7c424c4f434b434841494e5f52455345525645445f42595445535f454e447c"
 #define BLOCKCHAIN_RESERVED_BYTES_DATA_HASH "02800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000"
 
-// Network Ports and IPs
+// ===================== Network Ports and IPs =====================
 #define XCASH_DAEMON_PORT 18281
 #define XCASH_WALLET_PORT 18285
 #define XCASH_DPOPS_PORT 18283
@@ -35,7 +35,7 @@
 #define XCASH_DPOPS_IP "127.0.0.1"
 #define MAXIMUM_CONNECTIONS_IP_ADDRESS_OR_PUBLIC_ADDRESS 20
 
-// Network Block String
+// ===================== Network Block String =====================
 #define NETWORK_VERSION "0d0d"
 #define BLOCK_REWARD_INPUT "01"
 #define BLOCK_REWARD_TRANSACTION_VERSION "02"
@@ -48,7 +48,7 @@
 #define CONSENSUS_NODE_NETWORK_BLOCK_NONCE "11111111"
 #define BLOCK_PRODUCER_NETWORK_BLOCK_NONCE "00000000"
 
-// Placeholders for Block Template
+// ===================== Placeholders for Block Template =====================
 #define GET_BLOCK_TEMPLATE_BLOCK_VERIFIERS_SIGNATURE_DATA "303030..."
 #define GET_BLOCK_TEMPLATE_BLOCK_VERIFIERS_VRF_SECRET_KEY_DATA "303030..."
 #define GET_BLOCK_TEMPLATE_BLOCK_VERIFIERS_SIGNATURE "000000..."
@@ -58,7 +58,7 @@
 #define GET_BLOCK_TEMPLATE_BLOCK_VERIFIERS_VRF_PUBLIC_KEY_DATA "303030..."
 #define GET_BLOCK_TEMPLATE_BLOCK_VERIFIERS_VRF_PUBLIC_KEY "000000..."
 
-// Buffer Sizes
+// ===================== Buffer Sizes =====================
 #define IP_LENGTH 253
 #define BUFFER_SIZE_NETWORK_BLOCK_DATA 500
 #define BUFFER_SIZE 300000
@@ -75,7 +75,7 @@
 #define SEND_TIMEOUT 5000
 #define CONNECTION_TIMEOUT 3000
 
-// Hash and Key Lengths
+// ===================== Hash and Key Lengths =====================
 #define DATA_HASH_LENGTH 128
 #define XCASH_PUBLIC_ADDR_LENGTH 98
 #define XCASH_WALLET_LENGTH 98
@@ -90,7 +90,7 @@
 #define MD5_HASH_SIZE 32
 #define DB_HASH_SIZE 128
 
-// Blockchain Varint Lengths
+// ===================== Blockchain Varint Lengths =====================
 #define VARINT_DECODED_VALUE_END_1_BYTE 16383
 #define VARINT_DECODED_VALUE_END_2_BYTE 2097151
 #define VARINT_DECODED_VALUE_END_4_BYTE 34359738367
@@ -99,7 +99,7 @@
 #define VARINT_DECODED_VALUE_END_7_BYTE 72057594037927935
 #define VARINT_ENCODED_VALUE_END_7_BYTE 0xffffffffffffff7f
 
-// Database
+// ===================== Database =====================
 #define DATABASE_CONNECTION "mongodb://127.0.0.1:27017"
 #define DATABASE_NAME "XCASH_PROOF_OF_STAKE"
 #define DATABASE_NAME_DELEGATES "XCASH_PROOF_OF_STAKE_DELEGATES"
@@ -114,14 +114,15 @@
 #define ID_MAX_SIZE 256
 #define NUM_FIELDS 18
 
-// General Settings
+// ===================== General Settings =====================
 #define BITS_IN_BYTE 8
 #define BLOCK_TIME 5
 #define BLOCK_TIME_SEC (BLOCK_TIME*60)
+#define BLOCKS_PER_DAY_FIVE_MINUTE_BLOCK_TIME 288
 #define SEND_OR_RECEIVE_SOCKET_DATA_TIMEOUT_SETTINGS 3
 #define INVALID_RESERVE_PROOFS_SETTINGS 3
 
-// XCASH DPOPS
+// ===================== XCASH DPOPS =====================
 #define BLOCK_VERIFIERS_TOTAL_AMOUNT 100
 #define BLOCK_VERIFIERS_AMOUNT 50
 #define BLOCK_VERIFIERS_SETTINGS 3
@@ -137,12 +138,12 @@
 #define MAX_THREADS 10
 #define BLOCK_VERIFIERS_IP_ADDRESS_TOTAL_LENGTH 100
 
-// Constants
+// ===================== Constants =====================
 #define ALPHANUM_STRING "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 #define XCASH_OK 1
 #define XCASH_ERROR 0
 
-// Enums
+// ===================== Enums =====================
 typedef enum XCASH_DBS {
     XCASH_DB_DELEGATES = 0,
     XCASH_DB_STATISTICS = 1,
