e534c34b03c7ce3b39be8a813df25a790735587a
John D <az0006t@protonmail.com>
2025-03-23 11:31:18 -0400

    Update xcash_block.h

--- src/xcash_block/xcash_block.h
+++ src/xcash_block/xcash_block.h
@@ -5,11 +5,11 @@
 #include <stdbool.h>
 #include <jansson.h>
 #include "config.h"
+#include "globals.h"
 #include "macro_functions.h"
 #include "uv_net_multi.h"
 #include "xcash_net.h"
 #include "xcash_round.h"
-#include "variables.h"
 
 bool sync_block_producers(void);
 
