ba829a81908ff8bc73e3bff9ddd494cee0c3c78d
John D <az0006t@protonmail.com>
2025-03-28 19:17:29 -0400

    Updates

--- src/core/globals.c
+++ src/core/globals.c
@@ -8,7 +8,9 @@ delegates_t delegates_all[BLOCK_VERIFIERS_TOTAL_AMOUNT] = {0};
 char xcash_wallet_public_address[XCASH_PUBLIC_ADDR_LENGTH + 1] = {0};
 char current_block_height[BUFFER_SIZE_NETWORK_BLOCK_DATA] = {0};
 char previous_block_hash[BLOCK_HASH_LENGTH + 1] = {0};
-unsigned char secret_key_data[crypto_vrf_SECRETKEYBYTES + 1] = {0};
+
+//unsigned char secret_key_data[crypto_vrf_SECRETKEYBYTES + 1] = {0};
+
 char secret_key[VRF_SECRET_KEY_LENGTH + 1] = {0};
 char current_round_part[2] = "1";
 char current_round_part_backup_node[2] = "0";
--- src/core/globals.h
+++ src/core/globals.h
@@ -18,7 +18,9 @@ extern delegates_t delegates_all[BLOCK_VERIFIERS_TOTAL_AMOUNT];
 extern char xcash_wallet_public_address[XCASH_PUBLIC_ADDR_LENGTH + 1]; // xCash wallet public address
 extern char current_block_height[BUFFER_SIZE_NETWORK_BLOCK_DATA]; // The current block height
 extern char previous_block_hash[BLOCK_HASH_LENGTH+1]; // The previous block hash
-extern unsigned char secret_key_data[crypto_vrf_SECRETKEYBYTES+1]; // Holds the secret key for signing block verifier messages
+
+//extern unsigned char secret_key_data[crypto_vrf_SECRETKEYBYTES+1]; // Holds the secret key for signing block verifier messages
+
 extern char secret_key[VRF_SECRET_KEY_LENGTH+1]; // Holds the secret key text for signing block verifier messages
 extern char current_round_part[2]; // The current round part (1-4)
 extern char current_round_part_backup_node[2]; // The current main node in the current round part (0-5)
