466362c93b4941bf09fc58be7497b71c027daef0
John D <az0006t@protonmail.com>
2025-03-28 19:24:35 -0400

    Update xcash_initialize.h

--- src/xcash_initialize/xcash_initialize.h
+++ src/xcash_initialize/xcash_initialize.h
@@ -6,8 +6,6 @@
 #include "config.h"
 #include "globals.h"
 #include "structures.h"
-#include "node_functions.h"
-#include "db_sync.h"
 
 void print_starter_state(const arg_config_t* arg_config);
 bool init_processing(const arg_config_t* arg_config);
