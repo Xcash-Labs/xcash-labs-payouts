e3123e0b2e13b36133f93580bcd7e0c68ba0631e
John D <az0006t@protonmail.com>
2025-03-28 19:36:00 -0400

    Updates

---
+++ src/db/db_int/db_init.h
@@ -0,0 +1,19 @@
+#ifndef DB_INIT_H
+#define DB_INIT_H
+
+#include <mongoc/mongoc.h>
+#include <bson/bson.h>
+#include <stdbool.h>
+#include "string.h"
+
+#include "config.h"
+#include "globals.h"
+#include "macro_functions.h"
+//#include "db_functions.h"
+
+bool initialize_database(void);
+void shutdown_database(void);
+bool initialize_mongo_database(const char* mongo_uri, mongoc_client_pool_t** database_client_thread_pool);
+void shutdown_mongo_database(mongoc_client_pool_t** database_client_thread_pool);
+
+#endif
