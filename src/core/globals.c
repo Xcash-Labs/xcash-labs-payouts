5164ef57c416ceb36581b4a5612feac9e7b93612
John D <az0006t@protonmail.com>
2025-03-28 21:58:15 -0400

    Updates

--- src/core/globals.c
+++ src/core/globals.c
@@ -21,8 +21,6 @@ struct current_round_part_vote_data current_round_part_vote_data;
 struct current_block_verifiers_majority_vote current_block_verifiers_majority_vote;
 int replayed_round_settings = 0;
 
-
-
 mongoc_client_pool_t* database_client_thread_pool = NULL;
 
 pthread_rwlock_t rwlock;
@@ -48,11 +46,11 @@ block_verifiers_list_t next_block_verifiers_list;
 
 const NetworkNode network_nodes[] = {
     {"XCA1dd7JaWhiuBavUM2ZTJG3GdgPkT1Yd5Q6VvNvnxbEfb6JhUhziTF6w5mMPVeoSv3aa1zGyhedpaa2QQtGEjBo7N6av9nhaU", "xcashseeds.us",
-     "f681a933620c8e9e029d9ac0977d3a2f1d6a64cc49304e079458e3b5d2d4a66f", 1},
+     "f681a933620c8e9e029d9ac0977d3a2f1d6a64cc49304e079458e3b5d2d4a66f"},
     {"XCA1b6Sg5QVBX4jrctQ9SVUcHFqpaGST6bqtFpyoQadTX8SaDs92xR8iec3VfaXKzhYijFiMfwoM4TuYRgy6NXzn5titJnWbra", "xcashseeds.uk",
-     "63232aa1b020a772945bf50ce96db9a04242583118b5a43952f0aaf9ecf7cfbb", 1},
+     "63232aa1b020a772945bf50ce96db9a04242583118b5a43952f0aaf9ecf7cfbb"},
     // Sentinel value (empty entry to mark the end)
-    {NULL, NULL, NULL, 0}};
+    {NULL, NULL, NULL}};
 
 char* server_limit_IP_address_list;
 char* server_limit_public_address_list;
