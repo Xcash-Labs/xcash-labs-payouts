df4883b065cf7e998c5bd9b41337faddcd68d6d4
John D <az0006t@protonmail.com>
2025-03-28 21:32:00 -0400

    Update network_security_functions.h

--- src/network/network_security_functions/network_security_functions.h
+++ src/network/network_security_functions/network_security_functions.h
@@ -10,12 +10,12 @@
 #include "config.h"
 #include "macro_functions.h"
 #include "globals.h"
-#include "db_functions.h"
-#include "network_daemon_functions.h"
-#include "network_functions.h"
-#include "string_functions.h"
-#include "VRF_functions.h"
-#include "node_functions.h"
+//#include "db_functions.h"
+//#include "network_daemon_functions.h"
+//#include "network_functions.h"
+//#include "string_functions.h"
+//#include "VRF_functions.h"
+//#include "node_functions.h"
 
 int handle_error(const char *function_name, const char *message, char *result, char *string);
 bool is_valid_message_type(const char *message_settings, const char *valid_types[], size_t valid_types_count);
