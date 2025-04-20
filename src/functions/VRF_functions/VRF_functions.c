c47a3f63873b3b3e133cd9126ff339f9d52a2c36
John D <az0006t@protonmail.com>
2025-04-19 22:16:40 -0400

    Update VRF_functions.c

--- src/functions/VRF_functions/VRF_functions.c
+++ src/functions/VRF_functions/VRF_functions.c
@@ -28,14 +28,15 @@ Description: Generates a random public and private key, to be used for the signi
 void generate_key()
 {
   // Variables
-  unsigned char vrf_secret_key_data[crypto_vrf_SECRETKEYBYTES + 1];
-  unsigned char vrf_public_key_data[crypto_vrf_PUBLICKEYBYTES + 1];
-  char vrf_secret_key[VRF_SECRET_KEY_LENGTH + SMALL_BUFFER_SIZE];
-  char vrf_public_key[VRF_PUBLIC_KEY_LENGTH + SMALL_BUFFER_SIZE];
+  unsigned char vrf_secret_key_data[crypto_vrf_SECRETKEYBYTES];
+  unsigned char vrf_public_key_data[crypto_vrf_PUBLICKEYBYTES];
+  char vrf_secret_key_hex[(crypto_vrf_SECRETKEYBYTES * 2) + 1];
+  char vrf_public_key_hex[(crypto_vrf_PUBLICKEYBYTES * 2) + 1];
+  
   int count;
   int count2;
-  memset(vrf_secret_key, 0, sizeof(vrf_secret_key));
-  memset(vrf_public_key, 0, sizeof(vrf_public_key));
+  memset(vrf_secret_key_hex, 0, sizeof(vrf_secret_key_hex));
+  memset(vrf_public_key_hex, 0, sizeof(vrf_public_key_hex));
   memset(vrf_secret_key_data, 0, sizeof(vrf_secret_key_data));
   memset(vrf_public_key_data, 0, sizeof(vrf_public_key_data));
   if (create_random_VRF_keys((unsigned char *)vrf_public_key_data, (unsigned char *)vrf_secret_key_data) != 1 || crypto_vrf_is_valid_key((const unsigned char *)vrf_public_key_data) != 1)
@@ -46,16 +47,16 @@ void generate_key()
   // convert the VRF data to a string
   for (count2 = 0, count = 0; count2 < (int)crypto_vrf_SECRETKEYBYTES; count2++, count += 2)
   {
-    snprintf(vrf_secret_key + count, VRF_SECRET_KEY_LENGTH - 1, "%02x", vrf_secret_key_data[count2] & 0xFF);
+    snprintf(vrf_secret_key_hex + count, VRF_SECRET_KEY_LENGTH - 1, "%02x", vrf_secret_key_data[count2] & 0xFF);
   }
   for (count2 = 0, count = 0; count2 < (int)crypto_vrf_PUBLICKEYBYTES; count2++, count += 2)
   {
-    snprintf(vrf_public_key + count, VRF_PUBLIC_KEY_LENGTH - 1, "%02x", vrf_public_key_data[count2] & 0xFF);
+    snprintf(vrf_public_key_hex + count, VRF_PUBLIC_KEY_LENGTH - 1, "%02x", vrf_public_key_data[count2] & 0xFF);
   }
   COLOR_PRINT("\nPublic Key:", "green");
-  COLOR_PRINT(vrf_public_key, "green");
+  COLOR_PRINT(vrf_public_key_hex, "green");
   COLOR_PRINT("\nSecret Key:", "green");
-  COLOR_PRINT(vrf_secret_key, "green");
+  COLOR_PRINT(vrf_secret_key_hex, "green");
   return;
 }
 
