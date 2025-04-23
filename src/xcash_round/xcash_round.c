d1eacaf82fc8422ec472c059d16b7f2843aa0434
John D <az0006t@protonmail.com>
2025-04-23 11:10:49 -0400

    Update xcash_round.c

--- src/xcash_round/xcash_round.c
+++ src/xcash_round/xcash_round.c
@@ -116,7 +116,7 @@ xcash_round_result_t process_round(void) {
   response_t** responses = NULL;
   char* vrf_message = NULL;
   // This message is defines as NONRETURN and not responses is expected
-  if (generate_and_request_vrf_data_msg) == XCASH_OK) {
+  if (generate_and_request_vrf_data_msg == XCASH_OK) {
       DEBUG_PRINT("Generated VRF message: %s", vrf_message);
       if (xnet_send_data_multi(XNET_DELEGATES_ALL_ONLINE, vrf_message, &responses)) {
           DEBUG_PRINT("Message sent to all online delegates.");
