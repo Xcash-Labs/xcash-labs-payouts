d51aed0308cf2ef076ed830590353c38f592f28b
John D <az0006t@protonmail.com>
2025-04-29 23:04:59 -0400

    Update uv_net_server.c

--- src/network/uv_net_server/uv_net_server.c
+++ src/network/uv_net_server/uv_net_server.c
@@ -13,8 +13,8 @@ void on_client_close(uv_handle_t *handle) {
 }
 
 void check_if_ready_to_close(server_client_t *client) {
-  if (!client->closed && (client->sent_reply || client->write_timeout) && client->received_reply)
-    client->closed = true;  // prevent double close
+  if (!client->closed && (client->sent_reply || client->write_timeout) && client->received_reply) {
+    client->closed = true;
     INFO_PRINT("Round-trip with %s complete. Closing connection.", client->client_ip);
     uv_read_stop((uv_stream_t *)&client->handle);
     uv_close((uv_handle_t *)&client->handle, on_client_close);
