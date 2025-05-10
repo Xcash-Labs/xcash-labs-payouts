bae67ee3d4c131731aab74e96165527161e13dc6
John D <az0006t@protonmail.com>
2025-05-10 13:44:52 -0400

    Update autoinstaller.sh

--- scripts/autoinstaller/autoinstaller.sh
+++ scripts/autoinstaller/autoinstaller.sh
@@ -1314,7 +1314,8 @@ function update_xcash()
 {
   echo -ne "${COLOR_PRINT_YELLOW}Updating X-CASH (This Might Take A While)${END_COLOR_PRINT}"
   echo "XCASH_DPOPS_INSTALLATION_DIR is: ${XCASH_DPOPS_INSTALLATION_DIR}"
-  echo "XCASH_DIR is:" $XCASH_DIR
+  echo "XCASH_DIR is:" XCASH_DIR
+
 }
 
 function update_xcash_old()
