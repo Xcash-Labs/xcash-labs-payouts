407fcdb5bbfe84f320eaacb7e89b38529d8de367
John D <az0006t@protonmail.com>
2025-05-10 13:09:42 -0400

    Update autoinstaller.sh

--- scripts/autoinstaller/autoinstaller.sh
+++ scripts/autoinstaller/autoinstaller.sh
@@ -1305,21 +1305,17 @@ function update_packages()
 
 function set_installation_dir_owner()
 {
-
-# this hosed my install once, need to check out
-
+# this hosed my install once, need
 #  sudo chown -R "$USER":"$USER" ${XCASH_DPOPS_INSTALLATION_DIR} 2&> /dev/null || true
 #  sudo chown -R "$USER":"$USER" ${XCASH_BLOCKCHAIN_INSTALLATION_DIR} 2&> /dev/null || true
 }
 
 function update_xcash()
 {
-  echo -ne "${COLOR_PRINT_YELLOW}Updating X-CASH xxx (This Might Take A While)${END_COLOR_PRINT}"
-  echo "XCASH_DIR is set to: $XCASH_DIR"
-
+  echo -ne "${COLOR_PRINT_YELLOW}Updating X-CASH (This Might Take A While)${END_COLOR_PRINT}"
   if [ ! -d "$XCASH_DIR" ]; then
     cd "${XCASH_DPOPS_INSTALLATION_DIR}"
-    git clone --quiet "${XCASH_URL}"
+    git pull --quiet "${XCASH_URL}"
   fi
   cd "${XCASH_DIR}"
   data=$([ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | sed 's/\// /g') | cut -f1) ] && echo "1" || echo "0")
