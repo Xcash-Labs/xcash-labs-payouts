8c62fa2437829e3ec07f6202fae9b6c6f232e4ae
John D <az0006t@protonmail.com>
2025-05-21 10:01:22 -0400

    Update block_verifiers_functions.c

--- src/functions/block_verifiers_functions/block_verifiers_functions.c
+++ src/functions/block_verifiers_functions/block_verifiers_functions.c
@@ -167,6 +167,7 @@ bool add_vrf_extra_and_sign(char* block_blob_hex)
     free(block_blob_bin);
     return false;
   }
+  
 
   block_blob_bin[pos++] = TX_EXTRA_VRF_SIGNATURE_TAG;
   block_blob_bin[pos++] = 208;  // length of VRF blob (must match vrf_pos)
