611cb53476603e6a626835bfd6cfa675a8da2190
John D <az0006t@protonmail.com>
2025-05-28 19:14:11 -0400

    Update delegate_server_functions.h

--- src/functions/delegate_server_functions/delegate_server_functions.h
+++ src/functions/delegate_server_functions/delegate_server_functions.h
@@ -8,13 +8,6 @@
 #include "db_functions.h"
 #include "xcash_delegates.h"
 
-
-
-
-#include "macro_functions.h"
-#include "db_functions.h"
-#include "xcash_delegates.h"
-
 //int block_verifiers_add_reserve_proof_check_if_data_is_valid(const char* MESSAGE, struct reserve_proof* reserve_proof);
 //int add_reserve_proof_remove_previous_vote(const char* PUBLIC_ADDRESS_CREATE_RESERVE_PROOF_DATA);
 //void server_receive_data_socket_node_to_block_verifiers_add_reserve_proof(const int CLIENT_SOCKET, const char* MESSAGE);
