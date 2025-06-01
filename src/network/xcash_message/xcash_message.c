997331b034056202437bbf40daf8aa435a796d9a
John D <az0006t@protonmail.com>
2025-05-31 23:02:52 -0400

    Update xcash_message.c

--- src/network/xcash_message/xcash_message.c
+++ src/network/xcash_message/xcash_message.c
@@ -163,7 +163,7 @@ xcash_msg_t get_message_type(const char* data) {
     return XMSG_NONE;  // Handle NULL or empty data safely
   }
   for (int i = 0; i < XMSG_MESSAGES_COUNT; i++) {
-    if (strcmp(data, xcash_net_messages[i]) == 0) {
+    if (strncmp(data, xcash_net_messages[i], strlen(xcash_net_messages[i])) == 0) {
       return (xcash_msg_t)i;
     }
   }
