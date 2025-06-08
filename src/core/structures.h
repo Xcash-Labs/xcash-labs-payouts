3f58b12313dc75568c07f171be4cf39ca05fc294
John D <az0006t@protonmail.com>
2025-06-07 22:43:52 -0400

    Updates

--- src/core/structures.h
+++ src/core/structures.h
@@ -21,32 +21,6 @@ typedef struct {
     int online_status;
 } NetworkNode;
 
-// xcash-next __OLD__
-/*
-typedef struct  {
-    char public_address[XCASH_WALLET_LENGTH+1];
-    char total_vote_count[100];
-    char IP_address[IP_LENGTH+1];
-    char delegate_name[MAXIMUM_BUFFER_SIZE_DELEGATES_NAME+1];
-    char about[1025];
-    char website[256];
-    char team[256];
-    char shared_delegate_status[10];
-    char delegate_fee[11];
-    char server_specs[1025];
-    char block_verifier_score[10];
-    char online_status[11];
-    char block_verifier_total_rounds[10];
-    char block_verifier_online_total_rounds[10];
-    char block_verifier_online_percentage[10];
-    char block_producer_total_rounds[10];
-    char block_producer_block_heights[BUFFER_SIZE_BLOCK_HEIGHTS_DATA];
-    char public_key[VRF_PUBLIC_KEY_LENGTH+1];
-    char registration_timestamp[21];
-    char online_status_ck[11];
-} delegates_t;
-*/
-
 // xcash-next
 typedef struct  {
     char public_address[XCASH_WALLET_LENGTH+1];
