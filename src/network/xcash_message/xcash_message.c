866c5748c352e15f68e3d7c833c68603aacc6884
John D <az0006t@protonmail.com>
2025-06-13 11:20:29 -0400

    Update xcash_message.c

--- src/network/xcash_message/xcash_message.c
+++ src/network/xcash_message/xcash_message.c
@@ -226,6 +226,10 @@ void handle_srv_message(const char* data, size_t length, server_client_t* client
  
   xcash_msg_t msg_type = get_message_type(trans_type);
 
+  if (verify_ip(data, client->client_ip)) {
+    ERROR_PRINT("Failed to validate the IP came from a valid source");
+    return;
+  }
 
   if (is_walletsign_type(msg_type)) {
     if (verify_data(data) == XCASH_ERROR) {
