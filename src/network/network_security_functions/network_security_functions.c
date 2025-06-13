356e4c8cab156170ccc61f5aaa04d7490907bae3
John D <az0006t@protonmail.com>
2025-06-13 14:04:14 -0400

    Update network_security_functions.c

--- src/network/network_security_functions/network_security_functions.c
+++ src/network/network_security_functions/network_security_functions.c
@@ -304,7 +304,7 @@ int verify_the_ip(const char *message, const char *client_ip) {
   char filter_json[256] = {0};
   char resolved_ip[INET_ADDRSTRLEN] = {0};
 
-  INFO_PRINT("Message: %s", data);
+  INFO_PRINT("Message: %s", message);
 
   // 1. Extract the public address
   if (parse_json_data(message, "public_address", ck_public_address, sizeof(ck_public_address)) != XCASH_OK) {
