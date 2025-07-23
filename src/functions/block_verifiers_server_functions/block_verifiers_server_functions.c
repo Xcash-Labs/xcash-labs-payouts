9cc66bf56acd5d2160e3b3e8bc92537d4f75706c
John D <az0006t@protonmail.com>
2025-07-23 18:51:00 -0400

    Update block_verifiers_server_functions.c

--- src/functions/block_verifiers_server_functions/block_verifiers_server_functions.c
+++ src/functions/block_verifiers_server_functions/block_verifiers_server_functions.c
@@ -131,6 +131,8 @@ void server_receive_data_socket_node_to_node_vote_majority(const char* MESSAGE) 
     return;
   }
 
+  INFO_PRINT("******************** Sign: %s", vote_signature);
+
   if (strlen(vote_signature) == 0 || strncmp(vote_signature, XCASH_SIGN_DATA_PREFIX, sizeof(XCASH_SIGN_DATA_PREFIX) - 1) != 0) {
     ERROR_PRINT("Error with vote signature for %s", public_address);
     return;
