0acbeaf24314fbe534b5f401ea2bdbfbdd0374f9
John D <az0006t@protonmail.com>
2025-08-01 18:45:26 -0400

    Update network_security_functions.c

--- src/network/network_security_functions/network_security_functions.c
+++ src/network/network_security_functions/network_security_functions.c
@@ -17,10 +17,8 @@ Parameters:
 Return: 0 if an error has occured, 1 if successfull
 ---------------------------------------------------------------------------------------------------------*/
 int sign_data(char *message) {
-    INFO_PRINT("In sign start.......");
   const char *HTTP_HEADERS[] = {"Content-Type: application/json", "Accept: application/json"};
   const size_t HTTP_HEADERS_LENGTH = sizeof(HTTP_HEADERS) / sizeof(HTTP_HEADERS[0]);
-
   char *signature = calloc(XCASH_SIGN_DATA_LENGTH+1, sizeof(char));
   char *payload = calloc(MEDIUM_BUFFER_SIZE, sizeof(char));
   char *request = calloc(MEDIUM_BUFFER_SIZE * 2, sizeof(char));
@@ -63,9 +61,6 @@ int sign_data(char *message) {
     handle_error("sign_data", "Wallet signature failed", signature, payload, request);
     return XCASH_ERROR;
   }
-
-  INFO_PRINT("In sign.......");
-  INFO_PRINT("Signature: '%s' (length: %zu)", signature, strlen(signature));
 
   if (strlen(signature) != XCASH_SIGN_DATA_LENGTH ||
       strncmp(signature, XCASH_SIGN_DATA_PREFIX, strlen(XCASH_SIGN_DATA_PREFIX)) != 0) {
