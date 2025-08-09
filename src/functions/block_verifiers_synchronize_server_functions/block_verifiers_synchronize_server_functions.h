b14a381dca7611f928ca9c36d715b6ade94edf1b
John D <az0006t@protonmail.com>
2025-08-09 11:09:37 -0400

    Update block_verifiers_synchronize_server_functions.h

--- src/functions/block_verifiers_synchronize_server_functions/block_verifiers_synchronize_server_functions.h
+++ src/functions/block_verifiers_synchronize_server_functions/block_verifiers_synchronize_server_functions.h
@@ -15,7 +15,7 @@
 #include "xcash_message.h"
 #include "db_sync.h"
 
-void server_received_msg_get_sync_info(server_client_t* client, const char* MESSAGE);
+//void server_received_msg_get_sync_info(server_client_t* client, const char* MESSAGE);
 void server_receive_data_socket_node_to_network_data_nodes_get_current_block_verifiers_list(server_client_t* client);
 void server_receive_data_socket_node_to_node_db_sync_req(server_client_t *client, const char *MESSAGE);
 void server_receive_data_socket_node_to_node_db_sync_data(const char *MESSAGE);
