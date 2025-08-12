05dbe8672ff715150f1eb257ab06f26bc535e50c
John D <az0006t@protonmail.com>
2025-08-12 10:22:38 -0400

    Update network_security_functions.c

--- src/network/network_security_functions/network_security_functions.c
+++ src/network/network_security_functions/network_security_functions.c
@@ -154,6 +154,7 @@ int verify_data(const char *message) {
 
   if (strcmp(previous_block_hash, ck_previous_block_hash) != 0) {
     INFO_PRINT("Failed Signature Verification, previous block hash is not valid");
+    INFO_PRINT("previous_block_hash: %s     ck_previous_block_hash:%s", previous_block_hash, ck_previous_block_hash);
     return XCASH_ERROR;
   }
 
