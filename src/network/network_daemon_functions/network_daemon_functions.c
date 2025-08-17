30263b13e58234f34eb780cb718b8157ee7195eb
John D <az0006t@protonmail.com>
2025-08-17 13:39:47 -0400

    Clean up

--- src/network/network_daemon_functions/network_daemon_functions.c
+++ src/network/network_daemon_functions/network_daemon_functions.c
@@ -10,8 +10,7 @@ Parameters:
 Return:
   true if the blockchain is synced, false otherwise.
 ---------------------------------------------------------------------------------------------------------*/
-bool is_blockchain_synced(char target_height[BLOCK_HEIGHT_LENGTH],
-                          char height[BLOCK_HEIGHT_LENGTH]) {
+bool is_blockchain_synced(char target_height, char height) {
   const char* HTTP_HEADERS[] = {"Content-Type: application/json", "Accept: application/json"};
   const size_t HTTP_HEADERS_LENGTH = sizeof(HTTP_HEADERS) / sizeof(HTTP_HEADERS[0]);
   const char* RPC_ENDPOINT = "/get_info";
