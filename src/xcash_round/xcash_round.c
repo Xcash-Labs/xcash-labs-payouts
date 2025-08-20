c6a15fac2a50ddb1cf9cd3480cb4d46b0c40977a
John D <az0006t@protonmail.com>
2025-08-19 23:08:44 -0400

    Update xcash_round.c

--- src/xcash_round/xcash_round.c
+++ src/xcash_round/xcash_round.c
@@ -566,6 +566,7 @@ void start_block_production(void) {
               ERROR_PRINT("Blockchain is not synced");
               goto end_of_round_skip_block;
             }
+            INFO_PRINT("AFTER IN SEED NODE");
 
             uint64_t tmp_verifier_total_round = 0;
             uint64_t tmp_verifier_online_total_rounds = 0;
