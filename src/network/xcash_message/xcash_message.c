192e518027014880398e73fce397a00076e3fb0e
John D <az0006t@protonmail.com>
2025-08-31 12:22:30 -0400

    Update xcash_message.c

--- src/network/xcash_message/xcash_message.c
+++ src/network/xcash_message/xcash_message.c
@@ -33,7 +33,7 @@ bool is_walletsign_action_type(xcash_msg_t msg) {
 }
 
 // Create a message with key-value parameters
-char* create_message_param_list(xcash_msg_t msg, const char** pair_params) {
+char* create_message_param_list__OLD__(xcash_msg_t msg, const char** pair_params) {
   char message_buf[BUFFER_SIZE];
   memset(message_buf, 0, sizeof(message_buf));
 
@@ -58,7 +58,7 @@ char* create_message_param_list(xcash_msg_t msg, const char** pair_params) {
   // Check if message is signed
   if (is_walletsign_type(msg)) {
     if (sign_data(message_buf) != XCASH_OK) {
-      ERROR_PRINT("Failed to sign message: %s", xcash_net_messages[msg]);
+      ERROR_PRINT("Failed to sign message: %s", xcash_net_messages[msg]);    // jed
       return NULL;
     }
   }
@@ -240,7 +240,7 @@ void handle_srv_message(const char* data, size_t length, server_client_t* client
 
   if (is_walletsign_type(msg_type)) {
     if (verify_data(data) == XCASH_ERROR) {
-      ERROR_PRINT("Failed to validate message sign data");
+      WARNING_PRINT("Failed to validate message sign data");
       return;
     }
   }
