67d5ec3f84750af6f91a467a6f2ffe740836cc98
John D <az0006t@protonmail.com>
2025-09-04 10:51:49 -0400

    Update xcash_delegates.h

--- src/xcash_delegates/xcash_delegates.h
+++ src/xcash_delegates/xcash_delegates.h
@@ -2,7 +2,7 @@
 #define XCASH_DELEGATES_H
 
 #include "db_operations.h"
-#include "db_helpers.h"
+#include "db_functions.h"
 #include "globals.h"
 
 int read_organize_delegates(delegates_t* delegates, size_t* delegates_count_result);
