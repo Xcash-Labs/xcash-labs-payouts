fafebf7ba85bf5788b75d386edb6ab408e908844
John D <az0006t@protonmail.com>
2025-09-16 12:17:40 -0400

    Update xcash_message.c

--- src/network/xcash_message/xcash_message.c
+++ src/network/xcash_message/xcash_message.c
@@ -233,7 +233,6 @@ void handle_srv_message(const char* data, size_t length, server_client_t* client
  
   xcash_msg_t msg_type = get_message_type(trans_type);
 
-  
   if ((msg_type != XMSG_NODES_TO_BLOCK_VERIFIERS_REGISTER_DELEGATE) && 
     (msg_type != XMSG_NODE_TO_NETWORK_DATA_NODES_GET_CURRENT_BLOCK_VERIFIERS_LIST) &&
     (msg_type != XMSG_NODES_TO_BLOCK_VERIFIERS_VOTE) &&
