63b143781625d109e447e428ef37ed4b24c81bb5
John D <az0006t@protonmail.com>
2025-09-18 11:39:19 -0400

    Update block_verifiers_synchronize_server_functions.c

--- src/functions/block_verifiers_synchronize_server_functions/block_verifiers_synchronize_server_functions.c
+++ src/functions/block_verifiers_synchronize_server_functions/block_verifiers_synchronize_server_functions.c
@@ -243,13 +243,15 @@ void server_receive_data_socket_node_to_node_db_sync_data(const char *MESSAGE) {
     return;
   }
 
-  pthread_mutex_unlock(&delegates_all_lock);
-  bson_destroy(doc);
-
   if(add_indexes()) {
-    ERROR_PRINT("Failed to create index on ")
+    ERROR_PRINT("Failed to create index on delegates");
+    pthread_mutex_unlock(&delegates_all_lock);
+    bson_destroy(doc);
     return;
   }
+
+  pthread_mutex_unlock(&delegates_all_lock);
+  bson_destroy(doc);
 
   INFO_PRINT("Successfully updated delegates database from sync message");
 }
