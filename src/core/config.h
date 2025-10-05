2f5640759afe27f3c03541fe588e674afc853490
John D <az0006t@protonmail.com>
2025-10-05 17:56:36 -0400

    Update config.h

--- src/core/config.h
+++ src/core/config.h
@@ -213,7 +213,7 @@
 #define MAX_ACTIVE_CLIENTS 200
 
 #define WAKEUP_SKEW_SEC 10 
-
+#define JSON_INT_EPS 1e-9   /* small tolerance to treat numbers as integers */
 
 
 // ===================== Constants =====================
