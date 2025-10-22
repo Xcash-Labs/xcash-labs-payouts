db7d169700af04e8f2618f810e1ee0853203fb44
John D <az0006t@protonmail.com>
2025-10-22 10:56:55 -0400

    Clean up

--- src/db/db_sync/db_sync.h
+++ src/db/db_sync/db_sync.h
@@ -12,20 +12,6 @@
 #include "xcash_net.h"
 #include "xcash_delegates.h"
 
-typedef struct {
-  size_t block_height;  // the block height
-  bool db_reserve_bytes_synced;
-  char public_address[XCASH_WALLET_LENGTH + 1];
-  char db_hashes[DATABASE_TOTAL][DATA_HASH_LENGTH + 1];
-} xcash_node_sync_info_t;
-
-typedef struct
-{
-  xcash_node_sync_info_t* sync_info;
-  char overall_md5_hash[SHA256_HASH_SIZE + 1];
-
-} xcash_db_sync_prehash_t;
-
 bool hash_delegates_collection(char *out_hash_hex);
 bool fill_delegates_from_db(void);;
 int select_random_online_delegate(void);
