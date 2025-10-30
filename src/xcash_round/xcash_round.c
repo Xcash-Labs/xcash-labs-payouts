cccb19a6af36aa24ff53952e729e50581b8e16c8
John D <az0006t@protonmail.com>
2025-10-29 22:07:25 -0400

    More tweaks to is_blockchain_synced

--- src/xcash_round/xcash_round.c
+++ src/xcash_round/xcash_round.c
@@ -152,17 +152,8 @@ xcash_round_result_t process_round(void) {
 
   char target_height[BLOCK_HEIGHT_LENGTH + 1] = {0};
   char cheight[BLOCK_HEIGHT_LENGTH + 1] = {0};
-
   if (!is_blockchain_synced(target_height, cheight)) {
-    unsigned long long node_h = strtoull(cheight, NULL, 10);
-    unsigned long long target_h = strtoull(target_height, NULL, 10);
-    char target_disp[BLOCK_HEIGHT_LENGTH];
-    if (target_h == 0ULL || target_height[0] == '\0') {
-      strcpy(target_disp, "unknown");
-    } else {
-      snprintf(target_disp, sizeof(target_disp), "%llu", target_h);
-    }
-    WARNING_PRINT("Delegate is still syncing, node is at %llu and the target height is %s", node_h, target_disp);
+    ERROR_PRINT("Blockchain is not synced, skipping round");
     return ROUND_SKIP;
   }
 
