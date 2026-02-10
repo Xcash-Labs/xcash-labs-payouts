db924a535611c545e87d09d1be9542402f891a0f
John D <az0006t@protonmail.com>
2026-02-09 21:59:38 -0500

    Update init_processing.c

--- src/init_processing/init_processing.c
+++ src/init_processing/init_processing.c
@@ -219,10 +219,11 @@ bool print_starter_state(const arg_config_t *arg_config) {
     }
     pthread_mutex_unlock(&bans_lock);
 
-  } else {
-    ERROR_PRINT("Failed to retrieve IP for delegate, process aborting");
-    return false;
   }
+//   else {
+//    ERROR_PRINT("Failed to retrieve IP for delegate, process aborting");
+//    return false;
+//  }
 
   fprintf(stderr, "[%s] Daemon startup successful and is busy processing requests...\n\n", time_str);
   return true;
