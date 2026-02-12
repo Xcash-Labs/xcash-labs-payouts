dba9bf7601c9964dcd742c38259d6cf117a6a308
John D <az0006t@protonmail.com>
2026-02-12 17:17:43 -0500

    Update init_processing.h

--- src/init_processing/init_processing.h
+++ src/init_processing/init_processing.h
@@ -7,11 +7,9 @@
 #include "globals.h"
 #include "macro_functions.h"
 #include "structures.h"
-#include "node_functions.h"
 #include "db_sync.h"
 #include "string_functions.h"
 
 bool print_starter_state(const arg_config_t* arg_config);
-bool init_processing(const arg_config_t* arg_config);
 
 #endif // INIT_PROCESSING_H
