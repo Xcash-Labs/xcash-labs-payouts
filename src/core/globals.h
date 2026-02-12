48967ad61f684d9e9bd5b34b892e8682bbcb186d
John D <az0006t@protonmail.com>
2026-02-12 14:32:57 -0500

    Update globals.h

--- src/core/globals.h
+++ src/core/globals.h
@@ -27,8 +27,7 @@ extern delegates_timer_t delegates_timer_all[BLOCK_VERIFIERS_TOTAL_AMOUNT];
 extern char xcash_wallet_public_address[XCASH_WALLET_LENGTH + 1]; // xCash wallet public address
 extern char current_block_height[BLOCK_HEIGHT_LENGTH + 1]; // The current block height
 extern char previous_block_hash[BLOCK_HASH_LENGTH + 1]; // The previous block hash
-extern unsigned char secret_key_data[crypto_vrf_SECRETKEYBYTES]; // Holds the secret key for signing block verifier messages
-extern char secret_key[VRF_SECRET_KEY_LENGTH +1]; // Holds the secret key text for signing block verifier messages
+
 extern char vrf_public_key[VRF_PUBLIC_KEY_LENGTH + 1]; 
 extern char current_round_part[3]; // The current round part
 extern char delegates_hash[SHA256_HASH_SIZE + 1];
