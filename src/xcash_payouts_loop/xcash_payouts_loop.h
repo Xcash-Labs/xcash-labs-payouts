03e3aba03673f0bc33f0bde77b01b98e68c8faae
John D <az0006t@protonmail.com>
2026-02-12 16:54:42 -0500

    Update xcash_payouts_loop.h

--- src/xcash_payouts_loop/xcash_payouts_loop.h
+++ src/xcash_payouts_loop/xcash_payouts_loop.h
@@ -4,50 +4,8 @@
 #include <stdbool.h>
 #include <time.h> 
 #include "config.h"
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/time.h>
-#include <openssl/sha.h>
-#include <openssl/evp.h>
-#include <stdlib.h> 
-#include <pthread.h>
-#include "config.h"
 #include "globals.h"
-#include "macro_functions.h"
-#include "network_daemon_functions.h"
-#include "db_sync.h"
-#include "block_verifiers_functions.h"
-#include "string_functions.h"
 
-typedef struct {
-    char public_address[XCASH_WALLET_LENGTH + 1];
-    char IP_address[IP_LENGTH + 1];
-    char vrf_public_key[VRF_PUBLIC_KEY_LENGTH + 1];
-    char vrf_proof_hex[VRF_PROOF_LENGTH + 1];
-    char vrf_beta_hex[VRF_BETA_LENGTH + 1];
-    char vote_hash_hex[SHA256_EL_HASH_SIZE * 2 + 1];
-} producer_ref_t;
-
-extern producer_ref_t producer_refs[PRODUCER_REF_COUNT];
-
-typedef struct {
-    char public_address[XCASH_WALLET_LENGTH+1];
-    char IP_address[IP_LENGTH+1];
-    bool is_online;
-} producer_node_t;
-
-typedef struct
-{
-  size_t idx;                         // index in src_list
-  unsigned char beta[VRF_BETA_BYTES]; // decoded beta
-} committee_candidate_t;
-
-typedef enum {
-    ROUND_ERROR, // some system fault occurred. mostly communication errors or other non-fatal error.
-    ROUND_OK, //all the procedures finished successfully
-} xcash_round_result_t;
-
-xcash_round_result_t process_round(void);
-void start_block_production(void);
+void start_payouts_process(void);
 
 #endif
